11cf10d4a3216ab83e90a2e7503be4d69c2c06e1
Carolyn <yintingchien@gmail.com>
2022-08-12 12:06:06 -0400

    added nav bar logo and link

---
+++ client/src/component/Sign-In/Sign-In.component.jsx
@@ -0,0 +1,13 @@
+// const SignIn = () => {
+//   return
+//   <div className="sign-in-container">
+//     <div>
+//       <form>
+//         <label></label>
+//         <input></input>
+//         <label></label>
+//         <input></input>
+//       </form>
+//     </div>
+//   </div>
+// }
