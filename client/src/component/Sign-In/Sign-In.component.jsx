b4ed87ea03bb8f6414d41686417f042f53e1e0c6
Carolyn <yintingchien@gmail.com>
2022-08-12 15:09:18 -0400

    user signup and signIn page finished

--- client/src/component/Sign-In/Sign-In.component.jsx
+++ client/src/component/Sign-In/Sign-In.component.jsx
@@ -1,13 +1,47 @@
-// const SignIn = () => {
-//   return
-//   <div className="sign-in-container">
-//     <div>
-//       <form>
-//         <label></label>
-//         <input></input>
-//         <label></label>
-//         <input></input>
-//       </form>
-//     </div>
-//   </div>
-// }
+import { useState } from 'react'
+
+const initSignInfo = {
+  username: '',
+  password: ''
+}
+
+const SignIn = () => {
+  const [userInfo, setUserInfo] = useState(initSignInfo)
+  const { username, password } = userInfo
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    const { name, value } = e.target
+    setUserInfo({ ...userInfo, [name]: value })
+  }
+  console.log(userInfo)
+
+  return (
+    <div className="sign-container">
+      <form>
+        <div className="input-container">
+          <label>Username:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="username"
+            value={username}
+          />
+        </div>
+        <div className="input-container">
+          <label>Password:</label>
+          <input
+            type="password"
+            required
+            name="password"
+            onChange={handleChange}
+            value={password}
+          />
+        </div>
+        <button>Sign In</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ client/src/component/Sign-up/Sign-up.component.jsx
@@ -0,0 +1,124 @@
+import { useState } from 'react'
+
+const defaultFormfields = {
+  userName: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  phoneNumber: '',
+  password: ''
+}
+
+const SignUp = () => {
+  const [Formfields, setFormfields] = useState(defaultFormfields)
+  const [confirmPassword, setconfirmPassword] = useState('')
+  const [isError, setIsError] = useState('')
+  const { userName, firstName, lastName, email, phoneNumber, password } =
+    Formfields
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    const { name, value } = e.target
+
+    setFormfields({ ...Formfields, [name]: value })
+  }
+
+  const passwordValidte = (e) => {
+    console.log(e.target.value)
+    setconfirmPassword(e.target.value)
+    if (e.target.value !== password) {
+      setIsError(true)
+    } else {
+      setIsError('')
+    }
+  }
+
+  return (
+    <div className="sign-container">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>First Name:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="firstName"
+            value={firstName}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Last Name:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="lastName"
+            value={lastName}
+            required
+          />
+        </div>
+
+        <div className="input-container">
+          <label>Username:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="userName"
+            value={userName}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Email:</label>
+          <input
+            type="email"
+            onChange={handleChange}
+            name="email"
+            value={email}
+            required
+          />
+        </div>
+
+        <div className="input-container">
+          <label>Phone Number:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="phoneNumber"
+            value={phoneNumber}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+            required
+          />
+        </div>
+
+        <div className="input-container">
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            onChange={passwordValidte}
+            value={confirmPassword}
+            required
+          />
+        </div>
+
+        {isError && <p>password doesn't match</p>}
+        <button>Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
