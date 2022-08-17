eb5e02bd5f0aaee243662b08278bb19daeb217b8
v-jacx <jvelasquez6798@gmail.com>
2022-08-17 12:31:20 -0700

    create comment word wrap with css

--- client/src/component/CommentCard/CommentCard.component.jsx
+++ client/src/component/CommentCard/CommentCard.component.jsx
@@ -7,7 +7,7 @@ const CommentCard = ({ comment, location }) => {
     <div className="main-comment-container">
       <img src={location.images[1]} />
       <div className="comment-content-box">
-        <p>{comment ? comment.content : ''}</p>
+        <p id="textwrap">{comment ? comment.content : ''}</p>
       </div>
       <span>likes {comment ? comment.likes : ''}</span>
     </div>
